a8a022f4ee08dc3ec3041d668549dcdc75e17479
ahmed5768 <101082079+ahmed5768@users.noreply.github.com>
2024-07-23 02:23:33 +0500

    done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 // var roll = 0
+var StudentName = document.getElementById("name")
+var FatherName = document.getElementById("fatherName")
+var age = document.getElementById("age")
+var gender = document.getElementById("gender")
+var dob = document.getElementById("dob")
 
 var butnstu = document.getElementById("student")
 var table = document.createElement("table")
@@ -9,7 +14,7 @@ var tr = document.createElement("tr")
 
 table.appendChild(thead)
 thead.appendChild(tr)
-tr.innerHTML = `<th>Name</th> <th>Father Name</th> <th>Roll No</th> <th>Age</th> <th>Gender</th>`
+tr.innerHTML = `<th>Name</th> <th>Father Name</th> <th>Roll No</th> <th>Age</th> <th>Gender</th> <th>Date Of Birth</th> <th colspan="2">Action</th>`
 
 var tbody = document.createElement("tbody")
 table.appendChild(tbody)
@@ -17,12 +22,18 @@ table.appendChild(tbody)
 butnstu.parentNode.appendChild(table)
 
 butnstu.addEventListener("click", function(){
+    if(StudentName.value === "" || FatherName.value === "" || age.value === "" || gender.value === "" || dob.value === ""
+    ){
+        alert("Please fill your input")
+        return false
+    }
     var getData = JSON.parse(localStorage.getItem("studentData")) || []
     var data = {
-        StudentName: prompt("Enter your name "),
-        FatherName: prompt("Enter your fathername "),
-        age: prompt("Enter your age "),
-        gender: prompt("Enter a gender"),
+        StudentName: StudentName.value,
+        FatherName: FatherName.value,
+        age: age.value,
+        gender: gender.value,
+        dob: dob.value,
         rollno : Math.round(Math.random()*321)
         
     }
@@ -38,8 +49,24 @@ butnstu.addEventListener("click", function(){
         <td>${getData[i].rollno}</td>
         <td>${getData[i].age}</td>
         <td>${getData[i].gender}</td>
+        <td>${getData[i].dob}</td>
+        <td>Edit</td>
+        <td onclick="delete1(${i})" >Delete</td>
 
         `
         tbody.appendChild(bodyRow)
     }
 })
+
+var arr = [];
+var get = localStorage.getItem("studentData");
+if (get) {
+  arr = JSON.parse(get);
+}
+
+function delete1(e) {
+  console.log(e);
+  arr.splice(e, 1);
+  localStorage.setItem("studentData", JSON.stringify(arr));
+  event.target.parentNode.remove();
+}
